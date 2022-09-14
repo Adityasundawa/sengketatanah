e1c6476c4d0bf2931e0973307783f6d4b5e97008
adityasundawa <adityasundawa.co@gmail.com>
2022-09-14 21:53:29 +0700

    New Link

--- resources/views/layouts/template-admin/footer.blade.php
+++ resources/views/layouts/template-admin/footer.blade.php
@@ -1,13 +1,13 @@
   <!-- CoreUI and necessary plugins-->
 </div>
-  <script src="https://coreui.io/demos/bootstrap/4.0/free/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+  <script src="{{asset('/')}}/core-ui/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
     <!--[if IE]><!-->
-    <script src="https://coreui.io/demos/bootstrap/4.0/free/vendors/@coreui/icons/js/svgxuse.min.js"></script>
+    <script src="{{asset('/')}}/core-ui/vendors/@coreui/icons/js/svgxuse.min.js"></script>
     <!--<![endif]-->
     <!-- Plugins and scripts required by this view-->
-    <script src="https://coreui.io/demos/bootstrap/4.0/free/vendors/@coreui/chartjs/js/coreui-chartjs.bundle.js"></script>
-    <script src="https://coreui.io/demos/bootstrap/4.0/free/vendors/@coreui/utils/js/coreui-utils.js"></script>
-    <script src="https://coreui.io/demos/bootstrap/4.0/free/js/main.js"></script>
+    <script src="{{asset('/')}}/core-ui/vendors/@coreui/chartjs/js/coreui-chartjs.bundle.js"></script>
+    <script src="{{asset('/')}}/core-ui/vendors/@coreui/utils/js/coreui-utils.js"></script>
+    <script src="{{asset('/')}}/core-ui/js/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.js"
         integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <!-- <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script> -->
