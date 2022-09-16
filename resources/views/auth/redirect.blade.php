a0f03d6612f414c024e7183c9a8f166f4dece2a8
adityasundawa <adityasundawa.co@gmail.com>
2022-09-17 01:21:58 +0700

    Add dashboard

--- resources/views/auth/redirect.blade.php
+++ resources/views/auth/redirect.blade.php
@@ -20,7 +20,7 @@
     </span>
     <h3>Welcome Username</h3>
     @if(auth()->user()->role == "utama")
-    <a href="{{route('dashboard.index')}}">Klik Disini Untuk Melanjutkan</a>
+    <a href="{{route('utama.index')}}">Klik Disini Untuk Melanjutkan</a>
     @endif
     
     <script src="{{url('/')}}/loading/js/jquery-3.1.1.min.js"></script>
