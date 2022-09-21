47448b59e9c5040a1c656774ba8ad3d2205691dc
adityasundawa <adityasundawa.co@gmail.com>
2022-09-21 23:06:01 +0700

    Revisi Dashbpard korban 9

--- resources/views/korban/index.blade.php
+++ resources/views/korban/index.blade.php
@@ -68,7 +68,7 @@ use App\Models\User;
                 </div>
                 <div class="card-body">
                     @if($item['status_sengketa'] == 1)
-                    <div class="alert alert-warning" role="alert">
+                    <div class="alert alert-danger" role="alert">
                        Data anda telah kami terima dan menunggu petugas kami melakukan Verifikasi. Pastikan Telephone dapat di hubungi
                     </div>
                     @elseif($item['status_sengketa'] == 2)
