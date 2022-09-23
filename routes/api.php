02bde10fba46d4b938ecbc9d97e881e028e96da5
adityasundawa <adityasundawa.co@gmail.com>
2022-09-23 23:33:39 +0700

    ENV trosss

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routesssssssss
+| API Routessssssssss
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
