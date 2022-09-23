7cc11c07e8a1cfaf0f41ca70e6711e94763ee397
isa-ardi <isaardi21@gmail.com>
2022-09-23 18:58:58 +0700

    Penambahan Pilih Layanan Lain

--- resources/views/sengketa/all-data.blade.php
+++ resources/views/sengketa/all-data.blade.php
@@ -447,8 +447,8 @@
                                         <li><a class="dropdown-item ini" href="#">Konsultasi Tenaga Ahli Pertanahan</a>
                                         </li>
                                         <li><a class="dropdown-item ini" href="#">Edukasi Pertanahan</a></li>
-                                        <li><a class="dropdown-item ini" href="#">Pengamanan Lahan</a></li>
                                         <li><a class="dropdown-item ini" href="#">Jual Beli Lahan</a></li>
+                                        <li><a class="dropdown-item ini" href="#">Kerja Sama Lahan</a></li>
                                         <li><a class="dropdown-item ini" href="#">Pelelangan Lahan</a></li>
                                         <li><a class="dropdown-item ini" href="#">Jasa Tukang Ukur</a></li>
                                     </ul>
