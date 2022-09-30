0a446714c9480db0eabf31b87d044ffc76508803
isa-ardi <isaardi21@gmail.com>
2022-09-30 15:04:34 +0700

    jual beli lahan

--- resources/views/sengketa/index.blade.php
+++ resources/views/sengketa/index.blade.php
@@ -630,7 +630,7 @@
                         <div class="card rounded-0">
                             <div class="card-body p-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="45px" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><circle r="256" cx="256" cy="256" fill="#ffffff" shape="circle"></circle><g transform="matrix(0.7,0,0,0.7,76.79200973510743,76.80000000000001)"><path d="m498.195312 222.695312c-.011718-.011718-.023437-.023437-.035156-.035156l-208.855468-208.847656c-8.902344-8.90625-20.738282-13.8125-33.328126-13.8125-12.589843 0-24.425781 4.902344-33.332031 13.808594l-208.746093 208.742187c-.070313.070313-.140626.144531-.210938.214844-18.28125 18.386719-18.25 48.21875.089844 66.558594 8.378906 8.382812 19.445312 13.238281 31.277344 13.746093.480468.046876.964843.070313 1.453124.070313h8.324219v153.699219c0 30.414062 24.746094 55.160156 55.167969 55.160156h81.710938c8.28125 0 15-6.714844 15-15v-120.5c0-13.878906 11.289062-25.167969 25.167968-25.167969h48.195313c13.878906 0 25.167969 11.289063 25.167969 25.167969v120.5c0 8.285156 6.714843 15 15 15h81.710937c30.421875 0 55.167969-24.746094 55.167969-55.160156v-153.699219h7.71875c12.585937 0 24.421875-4.902344 33.332031-13.808594 18.359375-18.371093 18.367187-48.253906.023437-66.636719zm0 0" fill="#61481c" data-original="#000000" class=""></path></g></svg>
-                                <h6 class="mt-1 mb-1" style="font-size: 12px">HOME</h6>
+                                <h6 class="mt-0 mb-0" style="font-size: 12px">HOME</h6>
                             </div>
                         </div>
                         </button>
@@ -690,7 +690,7 @@
                                             </g>
                                         </g>
                                 </svg>
-                                <h6 class="mt-1 mb-1" style="font-size: 12px">PANDUAN</h6>
+                                <h6 class="mt-0 mb-0" style="font-size: 12px">PANDUAN</h6>
                             </div>
                         </div>
                         </button>
@@ -720,7 +720,7 @@
                                     </g>
                                     </g>
                                 </svg>
-                                <h6 class="mt-1 mb-1" style="font-size: 12px">JAMINAN</h6>
+                                <h6 class="mt-0 mb-0" style="font-size: 12px">JAMINAN</h6>
                             </div>
                         </button>
 
@@ -754,7 +754,7 @@
                                                     C361.013,137.082,340.026,126.94,331.035,126.94z" fill="#61481c" data-original="#000000" class=""></path>
                                             </g>
                                         </svg>
-                                    <h6 class="mt-1 mb-1" style="font-size: 12px;margin-top:13px">CHAT</h6>
+                                    <h6 class="mt-0 mb-0" style="font-size: 12px;margin-top:13px">CHAT</h6>
                                 </div>
                             </div>
                         </button>
@@ -786,7 +786,7 @@
                                     </g>
                                         </g>
                                     </svg>
-                                    <h6 class="mt-1 mb-1" style="font-size: 12px">TENTANG</h6>
+                                    <h6 class="mt-0 mb-0" style="font-size: 12px">TENTANG</h6>
                                 </div>
                             </div>
                         </button>
