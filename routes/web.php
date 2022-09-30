aeb997a99c2b3d0da6cf4c569068a5723afb26dd
hendra0ma <hendra0maulidan@gmail.com>
2022-09-30 18:00:09 +0700

    mangan mangan

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/', [SengketaController::class, 'index']);
 
 Route::post('sengketa/send', [SengketaController::class, 'store'])->name('sengketa.store');
 Route::get('bid-sengketa', [SengketaController::class, 'dataSengketa'])->name('bid-sengketa');
+Route::get('advokasi-pemerintah', [SengketaController::class, 'advokasiPemerintah'])->name('advokasi-pemerintah');
 Route::get('bid-sengketa/podcast', [SengketaController::class, 'podcastSengketa']);
 Route::get('form-korban', [SengketaController::class, 'formKorban']);
 Route::get('form-penyelesai', [SengketaController::class, 'formPenyelesai']);
@@ -54,6 +55,7 @@ Route::get('jasa-ppat', [PublicController::class, 'jasaPPAT'])->name('jasaPPAT')
 Route::get('join', [PublicController::class, 'joinSengketa'])->name('joinSengketa');
 Route::get('list-tukang', [PublicController::class, 'listTukang'])->name('listTukang');
 Route::get('detail-petugas', [PublicController::class, 'detailTukang'])->name('detailTukang');
+
 
 Route::get('register-jual-beli-lahan', [PublicController::class, 'registerJualBeli'])->name('registerJualBeli');
 Route::get('register-kerjasama', [PublicController::class, 'registerKerjasama'])->name('registerKerjasama');
