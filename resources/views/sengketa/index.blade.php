fca8e1d049d8a981d70eeef6e43a7f1206012e66
isa-ardi <isaardi21@gmail.com>
2022-10-01 10:25:25 +0700

    card pilihan

--- resources/views/sengketa/index.blade.php
+++ resources/views/sengketa/index.blade.php
@@ -673,8 +673,10 @@
                     <img src="{{asset('/uploads/sengketa-logo_pas-9.png')}}" style="width: 150px" alt="" srcset="" class="img-fluid">
                 </div>
             </div> --}}
+
+
             <div class="col-md text-center my-auto">
-                <style>
+                {{-- <style>
                     @media (max-width: 991.98px)) { 
                         .gambar-banner{
                             width: 75%
@@ -685,9 +687,28 @@
                             width: 90%
                         }
                      }
-                </style>
+                </style> --}}
 
-                <img src="{{asset('')}}uploads/banner_sengketa_tanah_1.png" class="gambar-banner" width="50%" alt="">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="{{asset('')}}uploads/banner_sengketa_tanah_1.png" class="d-block mx-auto" style="width: 95%" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="{{asset('')}}uploads/banner_sengketa_tanah_2.png" class="d-block mx-auto" style="width: 95%" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev desktop" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next desktop" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+                {{-- <img src="{{asset('')}}uploads/banner_sengketa_tanah_1.png" class="gambar-banner" width="50%" alt=""> --}}
                 {{-- <h1>Solusi Untuk Anda</h1>
                 <h3>Kami mempertemukan korban sengketa tanah dengan sponsor, pengacara dan
                     pemerintah dalam satu platform.</h3>
