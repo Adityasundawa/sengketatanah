bd2d49277d173b8eba556a5d45039adacc795e34
isa-ardi <isaardi21@gmail.com>
2022-10-02 21:16:55 +0700

    mengcloning

--- resources/views/sengketa/index.blade.php
+++ resources/views/sengketa/index.blade.php
@@ -11732,7 +11732,7 @@
 
                                 </style>
 
-                                <div id="carouselExampleIndicators" data-bs-touch="false" class="carousel slide mt-2 desktop" data-bs-ride="false">
+                                <div id="carouselExampleIndicators" data-bs-touch="false" class="carousel slide mt-2" data-bs-ride="false">
     
                                     <div class="carousel-inner desktop">
                                         <div class="carousel-item active">
@@ -11757,33 +11757,6 @@
                                         </div>
                                     </div>
 
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Previous</span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Next</span>
-                                    </button>
-                                </div>
-
-                                <div id="carouselExampleIndicators" data-bs-touch="false" class="carousel slide mt-2 mobile" data-bs-ride="false">
-
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img src="{{asset('')}}images/lahan1.jpeg" class="d-block image-kunam-2" alt="...">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="{{asset('')}}images/lahan2.jpeg" class="d-block image-kunam-2" alt="...">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="{{asset('')}}images/lahan3.jpeg" class="d-block image-kunam-2" alt="...">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="{{asset('')}}images/lahan4.jpeg" class="d-block image-kunam-2" alt="...">
-                                        </div>
-                                    </div>
-
                                     <div class="carousel-indicators custom scroll-simple">
                                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
                                             <img src="{{asset('')}}images/lahan1.jpeg" class="d-block w-100" alt="...">
@@ -11804,6 +11777,48 @@
                                         <span class="visually-hidden">Previous</span>
                                     </button>
                                     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
+                                </div>
+
+                                <div id="carouselExampleIndicatorsM" data-bs-touch="false" class="carousel slide mt-2" data-bs-ride="false">
+    
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="{{asset('')}}images/lahan1.jpeg" class="d-block image-kunam-2" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="{{asset('')}}images/lahan2.jpeg" class="d-block image-kunam-2" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="{{asset('')}}images/lahan3.jpeg" class="d-block image-kunam-2" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="{{asset('')}}images/lahan4.jpeg" class="d-block image-kunam-2" alt="...">
+                                        </div>
+                                    </div>
+
+                                    <div class="carousel-indicators custom scroll-simple">
+                                        <button type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
+                                            <img src="{{asset('')}}images/lahan1.jpeg" class="d-block w-100" alt="...">
+                                        </button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide-to="1" aria-label="Slide 2">
+                                            <img src="{{asset('')}}images/lahan2.jpeg" class="d-block w-100" alt="...">
+                                        </button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide-to="2" aria-label="Slide 3">
+                                            <img src="{{asset('')}}images/lahan3.jpeg" class="d-block w-100" alt="...">
+                                        </button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide-to="3" aria-label="Slide 4">
+                                            <img src="{{asset('')}}images/lahan4.jpeg" class="d-block w-100" alt="...">
+                                        </button>
+                                    </div>
+
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicatorsM" data-bs-slide="next">
                                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                         <span class="visually-hidden">Next</span>
                                     </button>
