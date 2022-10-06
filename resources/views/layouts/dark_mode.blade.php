28181dd0323fe15fa49c2ec1dd317ed90b7392fb
hendra0ma <hendra0maulidan@gmail.com>
2022-10-06 18:39:10 +0700

    Dark Mode Debug

--- resources/views/layouts/dark_mode.blade.php
+++ resources/views/layouts/dark_mode.blade.php
@@ -1,12 +1,16 @@
 <style>
     svg>circle {
         --darkreader-inline-fill: #FFFFFF !important;
-
     }
 
     path {
 
         --darkreader-inline-fill: #61481c !important;
+    }
+
+    .bg-dark-mode {
+        background-color: #1A1E29 !important;
+        --darkreader-inline-bgcolor: #1A1E29 !important
     }
 </style>
 
@@ -14,12 +18,76 @@
 <script src="https://cdn.jsdelivr.net/npm/darkreader@4.9.58/darkreader.min.js"></script>
 
 <script>
-    DarkReader.enable({
-        brightness: 100,
-        contrast: 100,
+    $(document).ready(function() {
+
+        if (localStorage.getItem('dark-mode') != undefined) {
+
+            DarkReader.enable({
+                brightness: 100,
+                contrast: 90,
+                sepia: 10
+            });
+            $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                el.classList.add('bg-dark-mode');
+                // console.log(el)
+            });
+            setTimeout(function() {
+                $('input#dark-mode').attr('checked', true);
+            }, 1000);
+
+
+
+        } else {
+            DarkReader.disable()
+            $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                el.classList.remove('bg-dark-mode');
+            });
+            setTimeout(function() {
+                $('input#dark-mode').attr('checked', false);
+            }, 1000);
+
+        }
     });
 
-    if (DarkReader.isEnabled()) {
+    $('input#dark-mode').on('click', function() {
+        const checkDark = $('input#dark-mode:checked').val()
+        if (checkDark == "on") {
+            DarkReader.enable({
+                brightness: 100,
+                contrast: 90,
+                sepia: 10
+            });
+            localStorage.removeItem('dark-mode')
+            setTimeout(function() {
+                localStorage.setItem('dark-mode', "on");
+                console.log(localStorage.getItem('dark-mode'));
+                $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                    el.classList.add('bg-dark-mode');
+                    // console.log(el)
+                });
+            }, 1000)
+        } else {
+            DarkReader.disable()
 
-    }
+            localStorage.removeItem('dark-mode')
+            setTimeout(function() {
+                console.log(localStorage.getItem('dark-mode'));
+                $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                    el.classList.remove('bg-dark-mode');
+                    // console.log(el)
+                });
+            }, 1000)
+        }
+    });
+    $(document).ready(function() {
+        if (DarkReader.isEnabled()) {
+            $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                el.classList.add('bg-dark-mode');
+            });
+        } else {
+            $('div.card-body.bg-card-dark-mode').each(function(i, el) {
+                el.classList.remove('bg-dark-mode');
+            });
+        }
+    });
 </script>
