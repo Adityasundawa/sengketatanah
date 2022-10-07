161c2383ad06ee310cbbba554ce3dbfa947079be
isa-ardi <isaardi21@gmail.com>
2022-10-07 17:34:19 +0700

    komit

--- resources/views/auth/bidder/pilih-l.blade.php
+++ resources/views/auth/bidder/pilih-l.blade.php
@@ -675,6 +675,9 @@
     @include('layouts.template-public.header')
 
     <div class="container mt-4 mb-5">
+        <center>
+            <img src="{{asset('')}}uploads/sengketa-logo_pas.png" alt="" style="width: 100px" class="mb-3">
+        </center>
         <h4 class="text-center">Pilih Jenis Akun Pemilik Lahan</h4>
         <h5 class="text-center text-muted fw-normal">Pilih Jenis Akun yang akan Anda Buat!</h5>
 
