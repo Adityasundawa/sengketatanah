b6627ee14855feb24ea265b574454afb3b5ac3b8
isa-ardi <isaardi21@gmail.com>
2022-10-07 16:55:23 +0700

    forming

--- resources/views/auth/bidder-pengacara/daftar.blade.php
+++ resources/views/auth/bidder-pengacara/daftar.blade.php
@@ -158,7 +158,7 @@
     @include('layouts.template-public.header')
 
     <div class="container mt-4 mb-5">
-        <h4 class="text-center">Form Sponsor (Personal)</h4>
+        <h4 class="text-center">Form Pengacara (Personal)</h4>
 
         <div class="row mt-5 text-center mote">
             <div class="col">
